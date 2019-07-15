68fc31f1e27500151872c5feaebec7ed511486ea
sabarivigneshn <sabarivignesh@redblacktree.com>
2019-07-15 19:22:55 +0530

    Created frontend folder

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as AppAction from '../redux/actions/AppAction'
+import TwitterLogin from 'react-twitter-auth/lib/react-twitter-auth-component.js';
+
 
 class Home extends Component {
     constructor(props) {
@@ -18,6 +20,12 @@ class Home extends Component {
         return (
             <div >
                 <h3>Home Component</h3>
+                <TwitterLogin
+                    loginUrl="http://localhost:4000/api/v1/auth/twitter"
+                    onFailure={this.onFailed}
+                    onSuccess={this.onSuccess}
+                    requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"
+                />
             </div>
         )
     }
